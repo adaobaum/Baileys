08139cdf5a37d0876c20db14cc8c399af5d39391
Renato Freitas <renatoiub@live.com>
2024-09-20 20:51:50 -0300

    Update messages-recv.ts

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -743,7 +743,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
                     if (ws.isOpen) {
 						 const msgId = msg.key.id!;
 						 const jid = jidNormalizedUser(msg.key.remoteJid!);
-						 console.log(node.attrs.type)
+						 
 
 						let retryCount = msgRetryCache.get<number>(msgId) || 0
 						if(retryCount >= maxMsgRetryCount) {
@@ -763,7 +763,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 							 await sendReceipt(jid!, participant!, [msg.key.id!], type);
 							await sendReceipt(jid!, undefined, [msg.key.id!], type);
 							 cleanMessage(msg, authState.creds.me!.id);
-							 msgRetryCache.del(msgId)
+							 
 							 await delay(1200);
 						
 
@@ -797,7 +797,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
                     if (ws.isOpen) {
 						 const msgId = msg.key.id!;
 						 const jid = jidNormalizedUser(msg.key.remoteJid!);
-						 console.log(node.attrs.type)
+					
 
 						let retryCount = msgRetryCache.get<number>(msgId) || 0
 						if(retryCount >= maxMsgRetryCount) {
@@ -817,7 +817,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 							 await sendReceipt(jid!, participant!, [msg.key.id!], type);
 							await sendReceipt(jid!, undefined, [msg.key.id!], type);
 							 cleanMessage(msg, authState.creds.me!.id);
-							 msgRetryCache.del(msgId)
+							
 							 await delay(1200);
 						
 
