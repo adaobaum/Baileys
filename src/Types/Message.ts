3ab2ff6ff79f7d5250d45c45bf2655b0cb0ee2d0
Renato Freitas <renatoiub@live.com>
2024-09-22 18:57:11 -0300

    Update Message.ts

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -210,6 +210,8 @@ export type MessageRelayOptions = MinimalRelayOptions & {
     useUserDevicesCache?: boolean
     /** jid list of participants for status@broadcast */
     statusJidList?: string[]
+    isretry?: boolean	
+	
 }
 
 export type MiscMessageGenerationOptions = MinimalRelayOptions & {
