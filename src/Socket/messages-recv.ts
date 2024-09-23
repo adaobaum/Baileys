3e8297170b409252a9e6d053624bb15ae91ed6bb
Renato Freitas <renatoiub@live.com>
2024-09-23 19:23:06 -0300

    Update messages-recv.ts

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -747,18 +747,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
         try {
 			while (!ws.isOpen) {
 			logger.error('Conexão com o socket fechada, aguardando a reconexão para decodificar a mensagem')
-			const error = new Error('Connection closed');
-  			(error as any).output = { statusCode: 408 }; // Adiciona o código 408
-			sendMessageAck(node)
-
-  			// Emite um evento de atualização de conexão com o código de status 408
-			ev.emit('connection.update', {
-				connection: 'close',
-				lastDisconnect: {
-				error: error, // Passa o erro com statusCode
-				date: new Date()
-				}
-			});
+			const error = new Error('Connection closed');  			
   			await delay(1000)
 			}			
 			
