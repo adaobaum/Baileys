6e29782228fe6fbe8d6b47a3b22a0f5b107c1f67
Renato Freitas <renatoiub@live.com>
2025-02-17 14:57:14 -0300

    Update encode.ts

--- src/WABinary/encode.ts
+++ src/WABinary/encode.ts
@@ -163,19 +163,21 @@ const encodeBinaryNodeInner = (
 
 		return true
 	}
-	
-	const isHex = (str) => {
-        	if (str.length > TAGS.PACKED_MAX) {
-            		return false;
-        	}
-        	for (const char of str) {
-            		const isInNibbleRange = char >= '0' && char <= '9';
-            		if (!isInNibbleRange && !(char >= 'A' && char <= 'F')) {
-                		return false;
-            		}
-        	}
-        	return true;
-    	};
+
+	const isHex = (str: string) => {
+		if(str.length > TAGS.PACKED_MAX) {
+			return false
+		}
+
+		for(const char of str) {
+			const isInNibbleRange = char >= '0' && char <= '9'
+			if(!isInNibbleRange && !(char >= 'A' && char <= 'F')) {
+				return false
+			}
+		}
+
+		return true
+	}
 
 	const writeString = (str: string) => {
 		const tokenIndex = TOKEN_MAP[str]
@@ -188,7 +190,7 @@ const encodeBinaryNodeInner = (
 		} else if(isNibble(str)) {
 			writePackedBytes(str, 'nibble')
 		} else if(isHex(str)) {
-			writePackedBytes(str, 'hex') 
+			writePackedBytes(str, 'hex')
 		} else if(str) {
 			const decodedJid = jidDecode(str)
 			if(decodedJid) {
